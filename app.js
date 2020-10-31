b66a75e07896365bb90a1205d1c3e2c3f5065a2a
Albert Padin <albert@symph.co>
2020-10-31 12:07:53 +0800

    Fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('<body style="{background: \'yellow\'}">Hello, GDG!</body>')
+    .send('<body style="{background-color: \'yellow\'}">Hello, GDG!</body>')
     .end();
 });
 
